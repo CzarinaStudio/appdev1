20db42184902b65b6e2e84f515943274785e8a1e
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-09-17 14:40:35 +0800

    Done responding to events

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,20 +1,14 @@
-function ItemList({ items }) {
-  return (
-    <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
-  );
-}
-
 function App() {
-  const fruits = ["Apple", "Banana", "Orange", "Mango"];
+  function handleClick() {
+    alert("You clicked me!");
+  }
 
   return (
     <div>
-      <h1>Rendering Lists</h1>
-      <ItemList items={fruits} />
+      <h1>Responding to Events</h1>
+      <button onClick={handleClick}>
+        Click me
+      </button>
     </div>
   );
 }
