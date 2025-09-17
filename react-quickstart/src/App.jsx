ed8a167975b0e9bd595a1f975e42f0ec4fd80c44
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-09-17 14:33:43 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,17 +1,11 @@
-function Greeting() {
-  return (
-    <div>
-      <h2>Hello from Greeting component!</h2>
-      <p>This is a nested component.</p>
-    </div>
-  );
-}
-
 function App() {
   return (
     <div>
-      <h1>Creating and Nesting Components</h1>
-      <Greeting />
+      <h1>Writing Markup with JSX</h1>
+      <div>
+        <h2>Hello JSX!</h2>
+        <p>This is JSX markup inside React.</p>
+      </div>
     </div>
   );
 }
