7c4afae8449c5a711e13422bb1414d1869b269f4
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-09-17 14:36:49 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,11 +1,25 @@
+function StyledBox() {
+  const boxStyle = {
+    width: "150px",
+    height: "50px",
+    backgroundColor: "lightgreen",
+    border: "2px solid green",
+    textAlign: "center",
+    lineHeight: "50px",
+    marginTop: "10px",
+  };
+
+  return <div style={boxStyle}>Styled Box</div>;
+}
+
 function App() {
   return (
     <div>
-      <h1>Writing Markup with JSX</h1>
-      <div>
-        <h2>Hello JSX!!!</h2>
-        <p>This is JSX markup inside React.</p>
-      </div>
+      <h1>Adding Styles</h1>
+      <p style={{ color: "blue", fontWeight: "bold" }}>
+        This text is styled inline with JSX.
+      </p>
+      <StyledBox />
     </div>
   );
 }
