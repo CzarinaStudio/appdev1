4e1af75863ebfdddd52baed8c1bc58242e6f07ef
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-10-26 08:58:43 +0800

    Done with how to use curly braces with JSX to access JavaScript functionality from your components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
 export default function TodoList() {
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
       <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
       />
       <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
       </ul>
-    </>
+    </div>
   );
 }
 
-// ii. End of Code: How to add markup to JavaScript with JSX
+
+// iv. End of Code: How to use curly braces with JSX to access JavaScript functionality from your components
