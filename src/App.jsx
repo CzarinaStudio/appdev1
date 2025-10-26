d869e00610d53a8ea8baf99972aa569a24ae186c
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-10-26 08:32:52 +0800

    Done with how to write your first React component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"; 
+
+//Code for: i. How to write your first React component
+function Profile() {
+  return (
+    <img
+      className="profile-pic" 
+      src="src\assets\Luna.jpg"
+      alt="luna"
+    />
+  );
+}
+export default Profile; 
+
+// End for: i. How to write your first React component
