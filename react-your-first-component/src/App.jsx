c1178615af492c223888900ac3e6cefd357f8210
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-10 02:57:45 +0800

    Done with defining a component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,13 @@
+// Defining a component
+
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  )
+}
+
+// Done with defining a component
+
