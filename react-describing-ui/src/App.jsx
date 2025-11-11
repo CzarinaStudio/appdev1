83d92fa075208756fe22933116b18317f961cd35
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-11 09:15:13 +0800

    Done with why understanding your UI as trees is useful (Root Module)

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,19 +1,16 @@
-// ix. Why understanding your UI as trees is useful
-import ComponentA from './ComponentA';
-import ComponentC from './ComponentC';
+// ix. Why understanding your UI as trees is useful (Root Module)
+import { greetA } from './ModuleA.js';
+import { greetB } from './ModuleB.js';
+import { greetC } from './ModuleC.js';
 
-function RootComponent() {
-  return (
-    <div>
-      <h1>Root Component</h1>
-      <ComponentA />
-      <ComponentC />
-    </div>
-  );
+export function rootGreet() {
+  console.log('Hello from RootModule');
+  greetA();
+  greetB();
+  greetC();
 }
 
-export default RootComponent;
-
+export default rootGreet;
 
 
 
