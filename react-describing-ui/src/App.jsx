a83bbff92df335e8e9ff1c701722c80aa5cbde2e
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-11 08:21:31 +0800

    Done with how to configure components with props

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,27 +1,38 @@
-// iv. How to use curly braces with JSX to access JavaScript functionality from your components
-export default function TodoList() {
-  const person = {
-    name: 'Gregorio Y. Zara',
-    theme: {
-      backgroundColor: 'black',
-      color: 'pink'
-    }
-  };
+// v. How to configure components with props
+import { getImageUrl } from './utils.js'
 
+export default function Profile() {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
+    <Card>
+      <Avatar
+        size={100}
+        person={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2'
+        }}
       />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
+    </Card>
+  );
+}
+
+function Avatar({ person, size }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+function Card({ children }) {
+  return (
+    <div className="card">
+      {children}
     </div>
   );
 }
+
 
