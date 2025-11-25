807e22d00ccc954d1207348d13c0b695d002edff
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-25 16:15:51 +0800

    Done with fetching todos data exercise using axios

--- react-useeffect-simple-api-fetch/src/App.jsx
+++ react-useeffect-simple-api-fetch/src/App.jsx
@@ -1,8 +1,9 @@
 // import UsersFetch from './UsersFetch.jsx'
-import PostsFetchAsync from './PostsFetchAsync.jsx';
+// import PostsFetchAsync from './PostsFetchAsync.jsx';
+import TodosFetchAxios from './TodosFetchAxios.jsx';
 
 export default function App() {
     return (
-        <PostsFetchAsync />
+        <TodosFetchAxios />
     )
 }
