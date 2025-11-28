9937c877d988e3a615d2971212a02de4d04367f1
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-28 17:54:46 +0800

    Done with Create the Login page section

--- react-todo-app-useeffect-router-redux/src/App.jsx
+++ react-todo-app-useeffect-router-redux/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
-// import Login from "./pages/Login"
+import Login from "./pages/Login"
 // import Todos from "./pages/Todos"
 function App () {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/todos" element={<Todos />} /> */}
       </Routes>
     </BrowserRouter>
