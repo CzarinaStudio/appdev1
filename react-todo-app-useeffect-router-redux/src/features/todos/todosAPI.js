75d43af6f066191e187be937d43f9030c01a1490
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-28 18:23:00 +0800

    Done with Create the Todos page section

--- react-todo-app-useeffect-router-redux/src/features/todos/todosAPI.js
+++
@@ -1,9 +0,0 @@
-export const getTodosAPI = function () {
-  return fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-    .then(res => res.json())
-}
-
-export const addTodoAPI = function () {
-  return fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(res => res.json())
-}
